aa6ecac13e03751400e5e435f5cb8ece96ba3568
Pinja Dodik <pinja@swarmia.com>
2022-11-27 16:09:38 +0200

    Adding date and time

--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
-//weather
+// weather
 
 function showWeather(response) {
   let h1 = document.querySelector("h1");
   let temperatureElement = document.querySelector("#current-temp");
+  let h2 = document.querySelector("h2");
 
   celsiusTemperature = response.data.temperature.current;
 
   h1.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+
+// date
+
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+if (hours < 10) {
+  hours = "0" + hours;
+}
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+h2.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}`;
 
 // search
 
