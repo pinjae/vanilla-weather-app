19521d8eee0ee866332f04db222c33fa92e13263
Pinja Dodik <pinja@swarmia.com>
2022-11-27 14:50:17 +0200

    Testing API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 // search
 
-function search(city) {
-  let city = document.querySelector("#city-input");
-  let apiKey = "1d34bfa5f4ff2d22f684fo0ete4b9039";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl);
+function showWeather(response) {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.city;
 }
 
-function handleSubmit(event) {
+function search(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
+  let cityInput = document.querySelector("#city-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${cityInput.value}`;
+  let apiKey = "1d34bfa5f4ff2d22f684fo0ete4b9039";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput.value}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl);
 }
 
 let form = document.querySelector("#search-form");
