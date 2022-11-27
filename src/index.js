4662c80a0d89d3e939d43fb2bc02980e46032e88
Pinja Dodik <pinja@swarmia.com>
2022-11-27 20:52:59 +0200

    Removing the H1 reference from the search function

--- src/index.js
+++ src/index.js
@@ -73,8 +73,6 @@ h2.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}`;
 function search(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${cityInput.value}`;
   let apiKey = "1d34bfa5f4ff2d22f684fo0ete4b9039";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput.value}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
